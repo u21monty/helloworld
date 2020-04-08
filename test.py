d9a43bc7410c145137c1eb0fd2b6e9f42cce8a1d
Yury Ivanov <yivanov@fb.com>
2020-04-08 16:01:32 -0400

    Test commit #1

--- test.py
+++ test.py
@@ -1,1 +1,2 @@
 print('Just a test line of code...')
+print('Here is another line! And another! One more!')
---
+++ test02.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+a = 9
+
+for number in range(a):
+	print('Hi there!')
